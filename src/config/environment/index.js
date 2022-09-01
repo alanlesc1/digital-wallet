a852823e85b2c3ae146484b14badb66754c7a324
Alan Lescano <alan.lesc1@gmail.com>
2022-09-01 16:26:19 -0300

    New email helper

--- src/config/environment/index.js
+++ src/config/environment/index.js
@@ -14,4 +14,12 @@ const serverPort = process.env.SERVER_PORT || 3000;
 
 const jwtSecret = process.env.JWT_SECRET;
 
-export { nodeEnv, serverPort, jwtSecret };
+const emailHost = process.env.EMAIL_HOST;
+
+const emailPort = process.env.EMAIL_PORT;
+
+const emailAuthUser = process.env.EMAIL_AUTH_USER;
+
+const emailAuthPassword = process.env.EMAIL_AUTH_PASSWORD;
+
+export { nodeEnv, serverPort, jwtSecret, emailHost, emailPort, emailAuthUser, emailAuthPassword };
